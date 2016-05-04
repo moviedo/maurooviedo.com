816465d849bd9bfe8d720e3658e19fa73ee10d35
Mauro Oviedo <mauro.o.2006@gmail.com>
2016-05-04 08:22:40 -0400

    Implemented dynamic title based on url.

--- app/app.js
+++ app/app.js
@@ -6,10 +6,21 @@ import router from './routes';
 var App = Vue.extend({
   components: {
     MauroNav: Nav,
+  },
+  methods: {
+    getTitle: getTitle
   }
 });
+
+
+function getTitle(path) {
+  if (path === '/')
+    return 'home';
+  else
+    return path.slice(1);
+}
 
 // Now we can start the app!
 // The router will create an instance of App and mount to
 // the element matching the selector.
-router.start(App, '#app-container');
+router.start(App, 'html');
