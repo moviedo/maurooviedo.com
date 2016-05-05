2b445813cf0612aa5328a83ff1ddd6cefed4199b
Mauro Oviedo <mauro.o.2006@gmail.com>
2016-05-05 13:35:27 -0400

    Added view design for displaying post information.

--- app/components/blog/post.vue
+++ app/components/blog/post.vue
@@ -1,23 +1,40 @@
 <script>
+  import model from './model';
+  
   export default {
-    
+    data: function() {
+      return model.fromPostHash(this.$route.params.blogToken);
+    }
   };
 </script>
 
 <template>
   <div class="container">
-    <div class="panel panel-default">
+    
+    <div v-if="title" class="panel panel-default">
       <div class="panel-heading">
         <h2 class="panel-title">{{ title }}</h2>
       </div>
       
       <p class="panel-body">
-        {{ post }}
+        {{{ post }}}
       </p>
       
       <div class="panel-footer">
         Posted: {{ date }}
       </div>
     </div>
+    
+    <div v-else class="panel panel-default">
+      <p class="panel-body">
+        PROBLEM FIND POST.
+      </p>
+    </div>
   </div>
 </template>
+
+<style lang="sass" scoped>
+  .container {
+    margin-top: 106px;
+  }
+</style>
