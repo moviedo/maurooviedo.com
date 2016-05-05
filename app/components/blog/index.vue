c3b1f0e45b7c87a18ae71df612a3ac848b6f319c
Mauro Oviedo <mauro.o.2006@gmail.com>
2016-05-04 23:06:27 -0400

    Added search and latest posts implementation to view.

--- app/components/blog/index.vue
+++ app/components/blog/index.vue
@@ -1,5 +1,14 @@
 <script>
+  import model from './model';
+  
   export default {
+    data: function() {
+      return {
+        query: undefined,
+        posts: model.allPosts(),
+        latestPosts: model.latestPosts()
+      }
+    }
   };
 </script>
 
@@ -7,11 +16,61 @@
   <section>
     <div class="jumbotron">
       <div class="text-center">
-        <h1>The Blog</h1>
+        <h1>Welcome</h1>
         <p>
-          Page under construction.
+          Web developer, programmer, and generally interested in JavaScript, Python, Elixir, and Dev Ops.
         </p>
       </div>
+    </div>
+    
+    <div class="container">
+      <div class="col-md-6 col-lg-6">
+        <form>
+          <div class="form-group">
+            <label for="search-post">
+              <h2>Search Posts</h2>
+            </label>
+            
+            <div class="input-group">
+              <input type="text"
+                  v-model="query"
+                  class="form-control"
+                  id="search-post"
+                  placeholder="Search for..."/>
+              <span class="input-group-btn">
+                <button class="btn btn-default" type="button">Search</button>
+              </span>
+            </div>
+            
+            <ul class="list-group" v-show="query">
+              <li class="list-group-item"
+                  v-for="post in posts | filterBy query in 'title'">
+                <a href="#">
+                  {{ post.title }}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </form>
+        
+      </div>
+      
+      <div class="col-md-6 col-lg-6">
+        <h2>Latest Posts</h2>
+        
+        <ul class="list-group"
+            v-for="(key, post) in latestPosts">
+          
+          <li class="list-group-item">
+            <a v-link="{ name: 'post', params: { blogToken: key } }">
+              {{ post.title }}
+            </a>
+            
+            Posted: <span>{{ post.date }}</span>
+          </li>
+        
+        </ul>
+      <div>
     </div>
   </section>
 </template>
