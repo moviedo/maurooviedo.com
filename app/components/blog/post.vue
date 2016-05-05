c6eb17da4e1419c78115e68afa000967d28ef2ce
Mauro Oviedo <mauro.o.2006@gmail.com>
2016-05-04 23:27:23 -0400

    Initial commit for Post component.

---
+++ app/components/blog/post.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+    
+  };
+</script>
+
+<template>
+  <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title">{{ title }}</h2>
+      </div>
+      
+      <p class="panel-body">
+        {{ post }}
+      </p>
+      
+      <div class="panel-footer">
+        Posted: {{ date }}
+      </div>
+    </div>
+  </div>
+</template>
